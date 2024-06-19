be1ab84337d5a4380fd60046b3dec725a4f3e4c0
SarahOtmane <sarahotmane02@gmail.com>
2024-06-19 14:30:43 +0200

    composant experiences valide

--- lib/screen/experience.dart
+++ lib/screen/experience.dart
@@ -1,31 +1,51 @@
 import 'package:flutter/material.dart';
 import '../data.dart';
 import '../components/experience_card.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ExperienceScreen extends StatelessWidget {
   const ExperienceScreen({super.key});
   
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Container(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Expériences',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 10),
-            ...experiences.map((experience) => Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10.0),
-              child: ExperienceCard(experience: experience),
-            )).toList(),
-          ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Expériences'),
+        backgroundColor: Colors.grey, // Fond gris pour l'AppBar
+        actions: [
+          IconButton(
+            icon: const FaIcon(FontAwesomeIcons.twitter),
+            onPressed: () {
+              // Action pour Twitter
+            },
+          ),
+          IconButton(
+            icon: const FaIcon(FontAwesomeIcons.facebook),
+            onPressed: () {
+              // Action pour Facebook
+            },
+          ),
+          IconButton(
+            icon: const FaIcon(FontAwesomeIcons.instagram),
+            onPressed: () {
+              // Action pour Instagram
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              ...experiences.map((experience) => Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: ExperienceCard(experience: experience),
+              )).toList(),
+            ],
+          ),
         ),
       ),
     );
