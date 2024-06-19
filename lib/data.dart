c09ca32b31a70bbf4589e7a29ab1a848875c4138
SarahOtmane <sarahotmane02@gmail.com>
2024-06-19 14:09:30 +0200

    update data files

--- lib/data.dart
+++ lib/data.dart
@@ -18,14 +18,14 @@ class Experience {
 
 List<Experience> experiences = [
   Experience(
-    imageName: 'assets/ca.svg',
+    imageName: 'assets/ca.png',
     companyName: 'Crédit Agricole',
     role: 'Développeuse Python',
     duration: '(09/2024-)',
     description: 'Développeuse pyhton dans l automatisation des tests',
   ),
   Experience(
-    imageName: 'assets/vegetal.jpeg',
+    imageName: 'assets/vegetal.png',
     companyName: 'Le végétal',
     role: 'Développeuse FullStack',
     duration: '(02/2024 - 06/2024)',
@@ -39,7 +39,7 @@ List<Experience> experiences = [
     description: 'Site vitrine pour la boite d\'audio visuelle',
   ),
   Experience(
-    imageName: 'assets/images/adidas.png',
+    imageName: 'assets/vegetal.png',
     companyName: 'Elephant Rose',
     role: 'Deveeloppeuse FullStack',
     duration: '(2023 - 2024)',
