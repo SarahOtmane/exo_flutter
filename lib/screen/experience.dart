577195e337a22194d27da20446988b00c7de16f6
SarahOtmane <sarahotmane02@gmail.com>
2024-06-19 14:03:16 +0200

    update composant

--- lib/screen/experience.dart
+++ lib/screen/experience.dart
@@ -1,12 +1,33 @@
-// screen/truc.dart
 import 'package:flutter/material.dart';
+import '../data.dart';
+import '../components/experience_card.dart';
 
 class ExperienceScreen extends StatelessWidget {
   const ExperienceScreen({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Text("Bonjour"),
+    return SingleChildScrollView(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              'Expériences',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            ...experiences.map((experience) => Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10.0),
+              child: ExperienceCard(experience: experience),
+            )).toList(),
+          ],
+        ),
+      ),
     );
   }
 }
