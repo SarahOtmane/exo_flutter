ac912d23835a6a8a28e59d0cf0653b21c0f10ce9
SarahOtmane <sarahotmane02@gmail.com>
2024-06-19 14:02:10 +0200

    composant experience card

---
+++ lib/components/experience_card.dart
@@ -0,0 +1,60 @@
+// widgets/experience_card.dart
+import 'package:flutter/material.dart';
+import '../data.dart'; // Assurez-vous que le chemin est correct
+
+class ExperienceCard extends StatelessWidget {
+  final Experience experience;
+
+  const ExperienceCard({super.key, required this.experience});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Image.asset(
+            experience.imageName,
+            width: 50,
+            height: 50,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  experience.companyName,
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '${experience.role} ${experience.duration}',
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  experience.description,
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
